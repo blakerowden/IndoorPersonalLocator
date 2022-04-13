f357e8a9ed07e7b3bc16363851a3fa841da19ecc
Liana van Teijlingen <s4580261@student.uq.edu.au>
2022-04-13 11:12:35 +1000

    prac1 specific code

---
+++ other/Liana_SCU/prac1/scu/build/zephyr/zephyr.dts.d
@@ -0,0 +1,13 @@
+empty_file.o: /home/liana/csse4011/zephyrproject/zephyr/misc/empty_file.c \
+ /home/liana/csse4011/zephyrproject/zephyr/boards/arm/thingy52_nrf52832/thingy52_nrf52832.dts \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/common/mem.h \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/arm/nordic/nrf52832.dtsi \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/liana/csse4011/zephyrproject/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/liana/csse4011/zephyrproject/zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/liana/csse4011/zephyrproject/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/liana/csse4011/zephyrproject/zephyr/dts/arm/nordic/override.dtsi \
+ app.overlay
---
+++ other/Liana_SCU/prac1/scu/src/main.c
@@ -0,0 +1,43 @@
+/**
+ ************************************************************************
+ * @file main.c
+ * @author Liana van Teijlingen
+ * @date 04.04.2021
+ * @brief Initialises scu threads
+ **********************************************************************
+ * */
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include <data/json.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+
+#include <init.h>
+#include <drivers/gpio.h>
+#include <sys/printk.h>
+#include "scu_ble.h"
+#include "scu_io.h"
+
+K_THREAD_DEFINE(ble_connect, 1024, thread_ble_connect, NULL, NULL, NULL, THREAD_PRIORITY_BLE_CONNECT_THREAD, 0, 0);
+K_THREAD_DEFINE(ble_discover, 1024, thread_ble_discover, NULL, NULL, NULL, THREAD_PRIORITY_BLE_CONNECT_THREAD, 0, 0);
+K_THREAD_DEFINE(send_message, 1024, thread_send_message, NULL, NULL, NULL, 20, 0, 0);
+K_THREAD_DEFINE(get_message, 4096, thread_get_message, NULL, NULL, NULL, 20, 0, 0);
+K_THREAD_DEFINE(read_button, 512, thread_read_button, NULL, NULL, NULL, 20, 0, 0);
+
+void main(void)
+{
+
+    led_gpio_enable();
+    init_buzzer();
+    init_hts();
+    init_lps();
+    init_ccs();
+    init_lis();
+}
