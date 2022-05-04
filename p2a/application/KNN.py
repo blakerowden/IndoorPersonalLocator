16c59f345d0c007faa9093f00a719c9eff98ee8e
Boston O'Neill <s4579873@student.uq.edu.au>
2022-05-04 18:12:59 +1000

    KNN

--- p2a/application/KNN.py
+++ p2a/application/KNN.py
@@ -9,11 +9,17 @@ from mqtt import *
 import logging
 import random
 from pathlib import Path
+import collections
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
+import pandas as pd
+from sklearn.model_selection import cross_val_predict
 
 DATAPATH = str(Path(__file__).parent / "Datapoints/datapoints")
 
-
-def compile_data(locations, rssi_list, file_read):
+def compile_data(loc_list, rssi_list, file_read):
     with open(file_read, "r") as file:
         csv_field_names = [
             "Pos_X",
@@ -53,19 +59,39 @@ def compile_data(locations, rssi_list, file_read):
         i = 0
         for row in csv_reader:
             if i != 0:
-                locations.append(row[:2])
-                rssi_list.append(row[2:])
+                if len(row) > 1:
+                    loc_list.append(row[:2])
+                    rssi_list.append(row[2:])
             i += 1
 
+def predict_pos(rssi_list):
 
-fileKNN = DATAPATH + 'KNN' + ".csv"
+    fileKNN = DATAPATH + 'KNN' + ".csv"
 
-loc_list = []
-rssi_list = []
+    rssi_list = []
+    loc_list = []
+    entire_iteration = 0
+    iteration = 1
+    class_list = []
 
-compile_data(loc_list, rssi_list, fileKNN)
+    compile_data(loc_list, rssi_list, fileKNN)
 
-print(loc_list)
+    for row in loc_list:
+        if len(row) > 1:
+            class_list.append(str(row[0]) + ',' + str(row[1]))
 
+    print(class_list)
+
+    print(len(class_list))
+    print(len(loc_list))
+    print(len(rssi_list))
+
+    X_train, X_test, Y_train, Y_test = train_test_split(rssi_list, class_list, test_size = .3, random_state = 4)
+
+    knn = KNeighborsClassifier(n_neighbors = 10)
+
+    knn.fit(X_train, Y_train)
+    predictions = knn.predict([rssi_list])
+    print(predictions)
 
 
